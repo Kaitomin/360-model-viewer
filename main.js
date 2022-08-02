24d2f6eaead9bac279dcb83a8d75c0b5201b2fc3
Kaito <hoangdai.d93@gmail.com>
2022-08-02 12:42:29 +0200

    Update

--- main.js
+++ main.js
@@ -1,35 +1,65 @@
+
+// DOM elements
 const inputRange = document.querySelector('#range')
 const img360 = document.querySelector('#main img')
 const prevButton = document.querySelector('#prev-img')
 const nextButton = document.querySelector('#next-img')
 
-function display360() {
+const currentProductImg = []
+let productsList = ''
+
+const display360 = () => {
   let rangeValue = inputRange.value
-  img360.src = `products/chair/${rangeValue}.jpg`
+  img360.src = `img/products/chair/${rangeValue}.jpg`
 }
 
-function prevImg() {
+const prevImg = () => {
   let rangeValue = inputRange.value
 
   if (rangeValue <= 1) return 
 
-  img360.src = `products/chair/${--rangeValue}.jpg`
+  img360.src = `img/products/chair/${--rangeValue}.jpg`
   inputRange.value = --inputRange.value
 }
-function nextImg() {
+
+const nextImg = () => {
   let rangeValue = inputRange.value
 
   if (rangeValue >= 40) return 
 
-  img360.src = `products/chair/${++rangeValue}.jpg`
+  img360.src = `img/products/chair/${++rangeValue}.jpg`
   inputRange.value = ++inputRange.value
 }
 
-inputRange.addEventListener('input', () => {
-  display360()
+// Preload all images of a product 
+const preloadCurrentProductImg = () => {
+  for (let i = 1; i <= 40; i++) {
+    let img = new Image()
+    img.src = `img/products/chair/${i}.jpg`
+    currentProductImg.push(img)
+  }
+}
+
+const fetchProducts = (name) => {
+  fetch('./data/products.json')
+    .then(res => res.json())
+    .then(data => productsList = data.products)
+}
+
+window.addEventListener('load', () => {
+  const products = document.querySelectorAll('.products-container div')
+
+  inputRange.addEventListener('input', display360)
+  inputRange.addEventListener('mouseup', e => inputRange.value = e.target.value)
+  prevButton.addEventListener('click', prevImg)
+  nextButton.addEventListener('click', nextImg)
+
+  console.log(products)
+  Array.from(products).forEach(p => p.addEventListener('click', (e) => {
+        
+  }))
+  
+  fetchProducts()
+  preloadCurrentProductImg()
 })
-inputRange.addEventListener('mouseup', (e) => {
-  inputRange.value = e.target.value
-})
-prevButton.addEventListener('click', prevImg)
-nextButton.addEventListener('click', nextImg)
+
